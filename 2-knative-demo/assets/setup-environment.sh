f475478fa101549c9972d9c5f580ecea02011923
Jim Curtis <jim.curtis@redhat.com>
2018-12-07 16:40:20 -0800

    Update setup-environment.sh

--- 2-knative-demo/assets/setup-environment.sh
+++ 2-knative-demo/assets/setup-environment.sh
@@ -73,23 +73,23 @@ oc apply -f istio-fixed.yaml
 until $(oc get pods -n istio-system | grep istio-sidecar-injector | grep 1/1 > /dev/null); do sleep 1; done
 
 # Install Kiali
-export JAEGER_URL="https://tracing-istio-system.2886795279-80-kitek03.environments.katacoda.com"; \
-export GRAFANA_URL="https://grafana-istio-system.2886795279-80-kitek03.environments.katacoda.com"; \
-export VERSION_LABEL="v0.9.0"
-curl https://raw.githubusercontent.com/kiali/kiali/${VERSION_LABEL}/deploy/openshift/kiali-configmap.yaml | \
-  VERSION_LABEL=${VERSION_LABEL} \
-  JAEGER_URL=${JAEGER_URL}  \
-  GRAFANA_URL=${GRAFANA_URL} envsubst | oc create -n istio-system -f -
-curl https://raw.githubusercontent.com/kiali/kiali/${VERSION_LABEL}/deploy/openshift/kiali-secrets.yaml | \
-  VERSION_LABEL=${VERSION_LABEL} envsubst | oc create -n istio-system -f -
-curl https://raw.githubusercontent.com/kiali/kiali/${VERSION_LABEL}/deploy/openshift/kiali.yaml | \
-  VERSION_LABEL=${VERSION_LABEL}  \
-  IMAGE_NAME=kiali/kiali \
-  IMAGE_VERSION=${VERSION_LABEL}  \
-  NAMESPACE=istio-system  \
-  VERBOSE_MODE=4  \
-  IMAGE_PULL_POLICY_TOKEN="imagePullPolicy: Always" envsubst | oc create -n istio-system -f -
-(oc get route kiali -n istio-system -o json|sed 's/80/443/')|oc apply -n istio-system -f -
+# export JAEGER_URL="https://tracing-istio-system.2886795279-80-kitek03.environments.katacoda.com"; \
+# export GRAFANA_URL="https://grafana-istio-system.2886795279-80-kitek03.environments.katacoda.com"; \
+# export VERSION_LABEL="v0.9.0"
+# curl https://raw.githubusercontent.com/kiali/kiali/${VERSION_LABEL}/deploy/openshift/kiali-configmap.yaml | \
+#   VERSION_LABEL=${VERSION_LABEL} \
+#   JAEGER_URL=${JAEGER_URL}  \
+#   GRAFANA_URL=${GRAFANA_URL} envsubst | oc create -n istio-system -f -
+# curl https://raw.githubusercontent.com/kiali/kiali/${VERSION_LABEL}/deploy/openshift/kiali-secrets.yaml | \
+#   VERSION_LABEL=${VERSION_LABEL} envsubst | oc create -n istio-system -f -
+# curl https://raw.githubusercontent.com/kiali/kiali/${VERSION_LABEL}/deploy/openshift/kiali.yaml | \
+#   VERSION_LABEL=${VERSION_LABEL}  \
+#   IMAGE_NAME=kiali/kiali \
+#   IMAGE_VERSION=${VERSION_LABEL}  \
+#   NAMESPACE=istio-system  \
+#   VERBOSE_MODE=4  \
+#   IMAGE_PULL_POLICY_TOKEN="imagePullPolicy: Always" envsubst | oc create -n istio-system -f -
+# (oc get route kiali -n istio-system -o json|sed 's/80/443/')|oc apply -n istio-system -f -
 
 # Disable mTLS in istio
 oc delete MeshPolicy default
